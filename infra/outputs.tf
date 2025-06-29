2b13b5b3584fc75c4f898f97f184eda562427644
Laurence MacGuire <leprechaun@gmail.com>
2025-06-29 20:31:18 +0700

    infra: rename the resource on the outputs too

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,6 +1,6 @@
 output "sqs_queue_arn" {
   description = "ARN of the SQS queue"
-  value       = aws_sqs_queue.s3_notification_queue.arn
+  value       = aws_sqs_queue.parsed_transaction_queue.arn
 }
 
 output "lambda_function_arn" {
